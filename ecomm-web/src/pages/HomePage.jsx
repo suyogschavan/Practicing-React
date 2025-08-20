5700d623674c81fe176c2669014d9741c29752d8
Suyog <suyogschavan03@gmail.com>
2025-08-20 18:07:14 +0530

    Update navigation links to use React Router paths

--- ecomm-web/src/pages/HomePage.jsx
+++ ecomm-web/src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ export function HomePage() {
     <title>Ecommerce Project</title>
       <div className="header">
         <div className="left-section">
-          <a href="index.html" className="header-link">
+          <a href="/" className="header-link">
             <img className="logo" src="images/logo-white.png" />
             <img className="mobile-logo" src="images/mobile-logo-white.png" />
           </a>
@@ -26,7 +26,7 @@ export function HomePage() {
             <span className="orders-text">Orders</span>
           </a>
 
-          <a className="cart-link header-link" href="checkout.html">
+          <a className="cart-link header-link" href="/checkout">
             <img className="cart-icon" src="images/icons/cart-icon.png" />
             <div className="cart-quantity">3</div>
             <div className="cart-text">Cart</div>
