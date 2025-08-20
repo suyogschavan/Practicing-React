64a86813fbafc6c68d71e18a20ffc65770fec619
Suyog <suyogschavan03@gmail.com>
2025-08-20 19:03:06 +0530

    Add Header component and refactor pages to use it for consistent navigation

--- ecomm-web/src/App.jsx
+++ ecomm-web/src/App.jsx
@@ -4,15 +4,19 @@ import { HomePage } from "./pages/HomePage";
 import { CheckoutPage } from "./pages/CheckoutPage";
 import { OrdersPage } from "./pages/OrdersPage";
 import { TrackingPage } from "./pages/TrackingPage";
+import { Header } from "./components/Header";
 
 function App() {
   return (
+    <>
+      <Header />
       <Routes>
-        <Route index element={<HomePage />}/>
-        <Route path="checkout" element={<CheckoutPage/>}/>
-        <Route path="orders" element={<OrdersPage/>}/>
-        <Route path="tracking" element={<TrackingPage/>}></Route>
+        <Route index element={<HomePage />} />
+        <Route path="checkout" element={<CheckoutPage />} />
+        <Route path="orders" element={<OrdersPage />} />
+        <Route path="tracking" element={<TrackingPage />}></Route>
       </Routes>
+    </>
   );
 }
 
