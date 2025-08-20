9e0ec340985ebbbf9f2635d887af99d24ccce1b9
Suyog <suyogschavan03@gmail.com>
2025-08-20 17:54:48 +0530

    Add React Router for navigation and update dependencies

--- ecomm-web/src/App.jsx
+++ ecomm-web/src/App.jsx
@@ -1,13 +1,14 @@
-import './App.css'
-import { HomePage } from './pages/HomePage'
+import { Routes, Route } from "react-router";
+import "./App.css";
+import { HomePage } from "./pages/HomePage";
 
 function App() {
-
   return (
-    <>
-     <HomePage/>
-    </>
-  )
+      <Routes>
+        <Route index element={<HomePage />}/>
+        <Route path="checkout" element={<div>Test checkout page</div>}/>
+      </Routes>
+  );
 }
 
-export default App
+export default App;
